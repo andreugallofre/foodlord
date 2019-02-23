7ab4031d6260fa71a65ab2dcc185a1e8e75eb37c
Albert Suarez <alsumo95@gmail.com>
2019-02-23 16:25:14 -0700

    Added upload image implementation

--- api/src/rapidapi/imagga.py
+++ api/src/rapidapi/imagga.py
@@ -1,7 +1,12 @@
+import base64
+import uuid
 import requests
 import os
 
+from PIL import Image
+
 from src import *
+from src.util import check, log
 
 IMAGGA_API_URL_TAGGING = 'https://imagga-api.p.rapidapi.com/tagging'
 IMAGGA_API_URL_CONTENT = 'https://imagga-api.p.rapidapi.com/content'
@@ -9,7 +14,7 @@ IMAGGA_API_URL_CONTENT = 'https://imagga-api.p.rapidapi.com/content'
 def get_info_url(url):
     headers = {
         'X-RapidAPI-Key': os.environ['RAPID_API_KEY']
-    } 
+    }
     data = {
         'url': url
     }
@@ -17,7 +22,7 @@ def get_info_url(url):
     response = requests.get(IMAGGA_API_URL_TAGGING, params=data, headers=headers)
     response_json = response.json()
 
-    # Extract the tags with more than 50% of confidence 
+    # Extract the tags with more than 50% of confidence
 
     if 'results' in response_json:
         result = []
@@ -28,20 +33,39 @@ def get_info_url(url):
         print (result)
     return None
 
-def get_info_image(image):
-    headers = {
-        'X-RapidAPI-Key': os.environ['RAPID_API_KEY'],
-        'Content-Type': 'application/x-www-form-urlencoded'
-    } 
-    data = {
-        'file': image
-    }
 
-    response = requests.get(IMAGGA_API_URL_CONTENT, params=data, headers=headers)
-    response_json = response.json()
+def upload_image(image_base64):
+    image_path = '/tmp/{}.jpg'.format(uuid.uuid4())
+    try:
+        # Save image
+        with open(image_path, 'wb') as file:
+            file.write(base64.urlsafe_b64decode(image_base64))
 
-    print(response_json)
+        # Resize image
+        img = Image.open(image_path)
+        if img.mode != 'RGB' or img.format == 'MPO':
+            img = img.convert('RGB')
+        img = img.resize((256, 256), Image.LANCZOS)
+        img.save(fp=image_path, format='JPEG', quality=95)
 
+        # Upload image
+        with open(image_path, 'rb') as file:
+            headers = {
+                'X-RapidAPI-Key': os.environ['RAPID_API_KEY']
+            }
+            data = {'filename': file}
+            response = requests.post(IMAGGA_API_URL_CONTENT, files=data, headers=headers)
+            response_json = response.json()
 
+        # Return image identifier
+        if check.exist('status', response_json) and response_json['status'] == 'success':
+            if check.exist('uploaded', response_json) and response_json['uploaded']:
+                return response_json['uploaded'][0]['id']
+        return None
 
-get_info_url('https://img-global.cpcdn.com/002_recipes/5af971b59aeac028/751x532cq70/bocata-de-jamon-foto-principal.jpg')
+    except Exception as e:
+        log.exception(e)
+        return None
+
+    finally:
+        os.remove(image_path)
