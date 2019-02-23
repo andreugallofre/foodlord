2f1e832866ec1649d8e723aadcc2af191ecf50a9
carlotacb <carkbra@gmail.com>
2019-02-23 22:28:51 +0100

    Structure created

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home/Home.js';
 
-class App extends Component {
+class App extends Component {  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Route path="/" exact component={Home} />
+        </div>
+      </Router>
     );
   }
 }
