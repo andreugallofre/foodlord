c962252b188279224f112797f482ff9b16160efc
Albert Suarez <alsumo95@gmail.com>
2019-02-23 22:14:42 -0700

    Added checks for post user endpoints

--- api/src/api/user.py
+++ api/src/api/user.py
@@ -1,3 +1,5 @@
+from flask import request
+
 from src.util import response
 
 
@@ -6,8 +8,18 @@ def get(user_id):
 
 
 def post():
+    body = request.json
+    required_parameters = ['first_name', 'last_name', 'email', 'username', 'password']
+    if not all(x in body for x in required_parameters):
+        return response.build(error=True, response='All request body parameters are required.')
+
     return response.build(error=False, response='OK')
 
 
 def login_post():
+    body = request.json
+    required_parameters = ['username', 'password']
+    if not all(x in body for x in required_parameters):
+        return response.build(error=True, response='All request body parameters are required.')
+
     return response.build(error=False, response='OK')
