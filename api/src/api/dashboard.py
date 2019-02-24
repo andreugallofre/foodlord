8f63ba1a90dbef1934a5d956d51f96e68af66644
Albert Suarez <alsumo95@gmail.com>
2019-02-24 03:41:59 -0700

    Implemented /dashboard endpoint

--- api/src/api/dashboard.py
+++ api/src/api/dashboard.py
@@ -1,28 +1,29 @@
-from flask import request
+import datetime
 
-from src.util import response
+from sqlalchemy import and_
+
 from src.model.report import Report
 from src.db.sqlalchemy import db_session
-
-from src.util import response
-from datetime import datetime, timedelta
+from src.util import response, log
 
 
 def get(user_id):
-    return response.build(error=False, response=user_id)
+    try:
+        week_date = datetime.datetime.today() - datetime.timedelta(days=7)
+        reports = db_session().query(Report).filter(and_(Report.time >= week_date, Report.username == user_id)).all()
 
+        response_dict = {}
+        while week_date.date() < datetime.datetime.now().date():
+            week_date = week_date + datetime.timedelta(days=1)
+            response_dict[week_date.strftime("%Y-%m-%d")] = 0.0
+        for report in reports:
+            response_dict[report.time.strftime("%Y-%m-%d")] += float(report.calories)
 
-def get_info_last_days():
-    d = datetime.datetime.today()
-    fromDate = datetime.strptime(d, '%Y-%m-%d')
-
-    day1 = db_session().query(Report).filter(datetime.utcfromtimestamp(Report.time).strftime('%Y-%m-%d') == fromDate).all()
-    day2 = db_session().query(Report).filter(datetime.utcfromtimestamp(Report.time).strftime('%Y-%m-%d') == fromDate-timedelta(days=1)).all()
-    day3 = db_session().query(Report).filter(datetime.utcfromtimestamp(Report.time).strftime('%Y-%m-%d') == fromDate-timedelta(days=2)).all()
-    day4 = db_session().query(Report).filter(datetime.utcfromtimestamp(Report.time).strftime('%Y-%m-%d') == fromDate-timedelta(days=3)).all()
-    day5 = db_session().query(Report).filter(datetime.utcfromtimestamp(Report.time).strftime('%Y-%m-%d') == fromDate-timedelta(days=4)).all()
-    day6 = db_session().query(Report).filter(datetime.utcfromtimestamp(Report.time).strftime('%Y-%m-%d') == fromDate-timedelta(days=5)).all()
-    day7 = db_session().query(Report).filter(datetime.utcfromtimestamp(Report.time).strftime('%Y-%m-%d') == fromDate-timedelta(days=6)).all()
-    
-
-    return None
+        response_list = []
+        for key, value in response_dict.items():
+            response_list.append({'date': key, 'calories': value})
+        return response.build(error=False, response=response_list)
+    except Exception as e:
+        log.error('Exception while getting data for the dashboard.')
+        log.exception(e)
+        return response.build(error=True, error_message='Unexpected error.')
