1c82562cabbe8cbd8c4ceb27950c1ef6c2d6e6af
Albert Suarez <alsumo95@gmail.com>
2019-02-23 22:28:52 -0700

    Fixed bugs in SQL scripts

--- db/create_ddl_foodlord.sql
+++ db/create_ddl_foodlord.sql
@@ -10,14 +10,14 @@ CREATE SEQUENCE foodlord_report_id_seq
 ALTER SEQUENCE foodlord_report_id_seq
     OWNER TO foodlord;
 
-CREATE SEQUENCE foodlord_ingredients_id_seq
+CREATE SEQUENCE foodlord_ingredient_id_seq
     INCREMENT 1
     START 1
     MINVALUE 1
     MAXVALUE 9223372036854775807
     CACHE 1;
 
-ALTER SEQUENCE foodlord_ingredients_id_seq
+ALTER SEQUENCE foodlord_ingredient_id_seq
     OWNER TO foodlord;
 
 
@@ -42,10 +42,10 @@ CREATE TABLE foodlord_report
 (
     id integer NOT NULL DEFAULT nextval('foodlord_report_id_seq'::regclass),
     name character varying(100) COLLATE pg_catalog."default",
-    calories float COLLATE pg_catalog."default",
+    calories numeric,
     username character varying(100) NOT NULL COLLATE pg_catalog."default",
     CONSTRAINT foodlord_report_pkey PRIMARY KEY (id),
-    CONSTRAINT foodlord_report_username_fkey FOREIGN KEY (project_usereport_usernamername)
+    CONSTRAINT foodlord_report_username_fkey FOREIGN KEY (username)
         REFERENCES foodlord_user (username) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE CASCADE
@@ -64,10 +64,10 @@ CREATE TABLE foodlord_ingredient
     id integer NOT NULL DEFAULT nextval('foodlord_ingredient_id_seq'::regclass),
     report_id integer NOT NULL,
     name character varying(100) COLLATE pg_catalog."default",
-    calories float COLLATE pg_catalog."default",
+    calories numeric,
     CONSTRAINT foodlord_ingredient_pkey PRIMARY KEY (id),
-    CONSTRAINT foodlord_report_username_fkey FOREIGN KEY (report_id)
-        REFERENCES foodlord_id (id) MATCH SIMPLE
+    CONSTRAINT foodlord_ingredient_report_fkey FOREIGN KEY (report_id)
+        REFERENCES foodlord_report (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE CASCADE
 )
