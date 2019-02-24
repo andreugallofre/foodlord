66778251d648354088763777c13ed0027a352402
carlotacb <carkbra@gmail.com>
2019-02-24 01:11:11 +0100

    Login complete without server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home/Home.js';
+import Login from './components/Login/Login.js'
 import Dashboard from './components/Dashboard/Dashboard.js'
 
 class App extends Component {  
@@ -10,6 +11,7 @@ class App extends Component {
         <div>
           <Route path="/" exact component={Home} />
           <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="/login" exact component={Login} />
         </div>
       </Router>
     );
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Form, FormField, TextInput, RoutedButton, Box, Button, Grommet, ResponsiveContext } from 'grommet';
+import './Login.css'
+
+class Login extends Component { 
+	constructor(props) {
+    super(props);
+    this.state = {
+      login: props.login,
+      username: String,
+      password: String
+    };
+		this.login = this.login.bind(this);
+		this.register = this.register.bind(this);
+	}
+
+	login() {
+
+    /* Insertar comprovació de que es correcte */ 
+
+		this.props.history.push("/");
+	}
+
+	register() {
+		this.props.history.push("/register");
+	}
+
+  render() {
+    return (
+    	<Grommet full>
+        <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill align='center' justify='center'>
+          <h2> Welcome to FoodLord </h2>
+            <Form>
+              <FormField name="username">
+                <TextInput autoFocus onChange={this.handleChange} placeholder="username"/>
+              </FormField>
+              <FormField name="password">
+                <TextInput type="password" onChange={this.handleChange} placeholder="******"/>
+              </FormField>
+            </Form>
+            <div>
+              <Button
+                label="Login"
+                onClick={this.login}
+              />
+            </div>
+            <div>
+              <RoutedButton primary={true} label="Register now" path="/register" />
+            </div>
+          </Box>
+        )}
+        </ResponsiveContext.Consumer>
+      </Grommet>
+    );
+  }
+}
+
+export default Login;
