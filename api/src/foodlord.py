049a69d1256882114ed260cf287c60609b915289
Albert Suarez <alsumo95@gmail.com>
2019-02-23 21:32:07 -0700

    Initialized DB with sqlalchemy

--- api/src/foodlord.py
+++ api/src/foodlord.py
@@ -2,6 +2,9 @@ import connexion
 
 from flask_cors import CORS
 
+from src.db import sqlalchemy
+from src.util import log
+
 
 connexion_app = connexion.FlaskApp(__name__, specification_dir='./openapi/')
 flask_app = connexion_app.app
@@ -9,6 +12,12 @@ flask_app.config['JSON_AS_ASCII'] = False
 connexion_app.add_api('openapi.yaml', arguments={'title': 'Food Lord API'})
 
 CORS(flask_app)
+
+
+@flask_app.teardown_appcontext
+def shutdown_session(exception=None):
+    log.debug('Session removed: {}'.format(exception))
+    sqlalchemy.db_session.remove()
 
 
 @flask_app.route('/')
